8fc860fd649ecab070a708e497853a44c2147c3b
Eva Szilardne Kallai <kallaiszilardne@gmail.com>
2022-12-07 12:18:41 +0100

    Showcase.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import SelectMovie from "./components/SelectMovie";
+import Showcase from "./components/Showcase";
 
 function App() {
   return (
     <div className="App">
       <SelectMovie />
+      <Showcase />
     </div>
   );
 }
