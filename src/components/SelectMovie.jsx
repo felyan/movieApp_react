a8cc34ebb8d92ea4455600b478b65561a29afc2c
Eva Szilardne Kallai <kallaiszilardne@gmail.com>
2022-12-07 11:26:03 +0100

    SelectMovie.jsx

--- src/components/SelectMovie.jsx
+++ src/components/SelectMovie.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Box, TextField, MenuItem } from '@mui/material'
 
 const SelectMovie = () => {
+  const [countries, setCountries] = useState([])
+  console.log(countries)
+
+  const handleChange = (event) => {
+    const value = event.target.value
+    setCountries(typeof value === 'string' ? value.split(',') : value)
+  }
   return (
-    <div>
-      <input type="text" />
-    </div>
+    <Box width='250px'  my={4}>
+      <TextField
+        label='Select Movie'
+        select
+        SelectProps={{
+          multiple: true
+        }}
+        size='small'
+        color='secondary'
+        helperText='Please select a Movie'
+        value={countries}
+        onChange={handleChange}>
+        <MenuItem value='10'>LoL</MenuItem>
+        <MenuItem value='12'>LoR</MenuItem>
+        <MenuItem value='11'>GoT</MenuItem>
+      </TextField>
+    </Box>
   )
 }
 
