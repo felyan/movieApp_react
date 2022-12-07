bf89350770afb39183e18e0d319f25a637bc21e2
Eva Szilardne Kallai <kallaiszilardne@gmail.com>
2022-12-07 11:04:49 +0100

    cra

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import SelectMovie from "./components/SelectMovie";
+
+function App() {
+  return (
+    <div className="App">
+      <SelectMovie />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SelectMovie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SelectMovie = () => {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  )
+}
+
+export default SelectMovie
